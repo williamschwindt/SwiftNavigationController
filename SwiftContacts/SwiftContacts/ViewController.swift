f40af23de90408254c2a02ba581a01cf61efdd10
William Schwindt <williammschwindt@gmail.com>
2020-03-04 10:11:40 -0800

    initial commit

---
+++ SwiftContacts/SwiftContacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftContacts
+//
+//  Created by William Schwindt on 3/4/20.
+//  Copyright © 2020 William Schwindt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
